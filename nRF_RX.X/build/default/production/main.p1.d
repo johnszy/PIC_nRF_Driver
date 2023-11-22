45249024caa0bcbd1b0ada9e141dd9a8b004a812
johnszy <johnszy@icloud.com>
2023-11-22 10:08:49 -0800

    Initial commit with RX test code for PCB nRF24L01 Driver Rev A

---
+++ nRF_RX.X/build/default/production/main.p1.d
@@ -0,0 +1,11 @@
+build/default/production/main.p1:  \
+main.c  \
+mcc_generated_files/mcc.h  \
+mcc_generated_files/device_config.h  \
+mcc_generated_files/pin_manager.h  \
+mcc_generated_files/interrupt_manager.h  \
+mcc_generated_files/spi.h  \
+mcc_generated_files/ext_int.h  \
+mcc_generated_files/eusart.h  \
+nRF24L01.h  \
+wl_module.h 
---
+++ nRF_RX.X/build/default/production/wl_module.p1.d
@@ -0,0 +1,11 @@
+build/default/production/wl_module.p1:  \
+wl_module.c  \
+wl_module.h  \
+mcc_generated_files/mcc.h  \
+mcc_generated_files/device_config.h  \
+mcc_generated_files/pin_manager.h  \
+mcc_generated_files/interrupt_manager.h  \
+mcc_generated_files/spi.h  \
+mcc_generated_files/ext_int.h  \
+mcc_generated_files/eusart.h  \
+nRF24L01.h 
